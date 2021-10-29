4f5682128ef55b8adce22b1e33a329e30f33b92d
Nishinoyama <begoingto1414@gmail.com>
2021-10-29 23:40:20 +0900

    fix: LJ illusion!!!

--- src/tet4wren/environment/Tetrimino.java
+++ src/tet4wren/environment/Tetrimino.java
@@ -1,7 +1,7 @@
 package tet4wren.environment;
 
 public enum Tetrimino implements Mino {
-    I(0), O(1), T(2), J(3), L(4), S(5), Z(6),;
+    I(0), O(1), T(2), L(3), J(4), S(5), Z(6),;
 
     private final int id;
     Tetrimino(int id) {

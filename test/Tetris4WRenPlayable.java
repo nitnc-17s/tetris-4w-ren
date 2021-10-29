c21242648794de95372cbb67d80611ec08656da3
Nishinoyama <begoingto1414@gmail.com>
2021-10-29 23:42:24 +0900

    new: now, we can play Tetris4WRen!

---
+++ test/Tetris4WRenPlayable.java
@@ -0,0 +1,64 @@
+import tet4wren.environment.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tetris4WRenPlayable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose your seed:");
+        long seed = scanner.nextLong();
+        MinoHolder minoHolder = new MinoHolder(3, new SevenBag(seed));
+        BlockState blockState = BlockState.B3;
+        int renCount = -1;
+
+        while (true) {
+            Tetrimino current = (Tetrimino) minoHolder.getCurrentMino();
+            Tetrimino hold = (Tetrimino) minoHolder.getHoldMino();
+            ArrayList<BlockState> nextStates = new ArrayList<>();
+            boolean consumeCurrent = true;
+            int currentBound = 0;
+            for (TetriminoPlacement placement : TetriminoPlacement.placementsFromTetrimino(current)) {
+                BlockState next = blockState.nextState(placement);
+                if ( next != null ) {
+                    nextStates.add(next);
+                    currentBound++;
+                }
+            }
+            for (TetriminoPlacement placement : TetriminoPlacement.placementsFromTetrimino(hold)) {
+                BlockState next = blockState.nextState(placement);
+                if ( next != null ) {
+                    nextStates.add(next);
+                }
+            }
+            if (nextStates.isEmpty()) {
+                break;
+            }
+
+            System.out.println(minoHolder);
+            System.out.println("Current Block State:");
+            System.out.println(blockState.toBlockAscii());
+            for (int i = 0; i < nextStates.size(); i++) {
+                System.out.println("" + i + " (" + (i < currentBound ? current : hold) + ") :");
+                System.out.println(nextStates.get(i).toBlockAscii());
+            }
+
+            BlockState nextState = null;
+            while (nextState == null) {
+                System.out.print("Choose next State:");
+                int index = scanner.nextInt();
+                if (index < nextStates.size()) {
+                    nextState = nextStates.get(index);
+                    consumeCurrent = index < currentBound;
+                }
+            }
+
+            Mino mino = minoHolder.consume(consumeCurrent);
+            System.out.println("vvv USAGE: " + mino + " vvv");
+            blockState = nextState;
+            renCount++;
+        }
+
+        System.out.println("End: " + renCount + "REN\n");
+    }
+}

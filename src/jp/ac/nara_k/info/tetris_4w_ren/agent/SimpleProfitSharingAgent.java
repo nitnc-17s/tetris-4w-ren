09f9e21de41a528f060b016ae2b2fab6079a53f8
Nishinoyama <begoingto1414@gmail.com>
2021-11-23 14:44:38 +0900

    Refactor redundant this keyword.

--- src/jp/ac/nara_k/info/tetris_4w_ren/agent/SimpleProfitSharingAgent.java
+++ src/jp/ac/nara_k/info/tetris_4w_ren/agent/SimpleProfitSharingAgent.java
@@ -33,24 +33,24 @@ public class SimpleProfitSharingAgent extends Agent {
         int ren = -1;
         while (!environment.isFinalState()) {
             ren++;
-            int state = this.environment.state();
-            int action = this.selectAction(state);
-            this.rules.add(new StateActionTuple(state, action));
+            int state = getState();
+            int action = selectAction(state);
+            rules.add(new StateActionTuple(state, action));
             if (rules.size() > rulesEffectLength) {
                 rules.poll();
             }
-            double reward = this.environment.action(action);
+            double reward = environment.action(action);
             if (reward < 0) {
-                for( StateActionTuple stateActionTuple : this.rules) {
+                for (StateActionTuple stateActionTuple : rules) {
                     int s = stateActionTuple.state();
                     int a = stateActionTuple.action();
-                    this.qTable[s][a] = this.qTable[s][a] + this.cBid * ( reward - this.qTable[s][a] );
+                    qTable[s][a] = qTable[s][a] + cBid * ( reward - qTable[s][a] );
                 }
-                this.rules.clear();
+                rules.clear();
             }
         }
-        if (this.isRecordRenResultsFlag()) {
-            this.renResults.add(ren);
+        if (isRecordRenResultsFlag()) {
+            renResults.add(ren);
         }
     }
 
@@ -86,7 +86,7 @@ public class SimpleProfitSharingAgent extends Agent {
     }
 
     public void clearRenResults() {
-        this.renResults.clear();
+        renResults.clear();
     }
 
     public List<Integer> getRenResults() {
@@ -99,12 +99,12 @@ public class SimpleProfitSharingAgent extends Agent {
     }
 
     private int softmaxSelectAction(int state) {
-        int actionLength = this.qTable[state].length;
+        int actionLength = qTable[state].length;
         double[] softmaxAccumulations = new double[actionLength+1];
         for (int i = 0; i < actionLength; i++) {
-            softmaxAccumulations[i+1] = softmaxAccumulations[i] + Math.exp(qTable[state][i] / this.temperature);
+            softmaxAccumulations[i+1] = softmaxAccumulations[i] + Math.exp(qTable[state][i] / temperature);
         }
-        double selection = this.randomGenerator.nextDouble() * softmaxAccumulations[actionLength];
+        double selection = randomGenerator.nextDouble() * softmaxAccumulations[actionLength];
         for (int i = 0; i < actionLength; i++) {
             if (selection < softmaxAccumulations[i+1]) return i;
         }
@@ -113,4 +113,3 @@ public class SimpleProfitSharingAgent extends Agent {
 }
 
 record StateActionTuple(int state, int action) {}
-
